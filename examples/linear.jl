84da97e7936112e5bdd88748fa44507eec1bdfe2
JG Brasier <brasier.jg@gmail.com>
2021-06-17 12:07:10 +0200

    Backprop through KF architecture

--- examples/linear.jl
+++ examples/linear.jl
@@ -1,12 +1,12 @@
 using Revise, KFEstimate
 using LinearAlgebra, Plots
-using Flux.Optimise
+using Flux, Flux.Optimise
 using ProgressBars
 pathof(KFEstimate)
 ## Normal Kalman Filter
 
 # dynamic model x = [p, v, a]
-dt = 0.001
+dt = 0.0001
 A = [1.0 dt 1/2*dt^2; 0.0 1.0 dt; 0.0 0.0 1.0]
 B = [0.0; 0.0; 1.0]
 B = reshape(B, length(B), 1)
@@ -22,7 +22,7 @@ kf = KalmanFilter(A, B, Q, H, R)
 # run simulation
 time_step = 0:1000
 x0 = [0.0; 0.0; 0.0]
-action_sequence = [[exp(-0.01*(t-1))] for t in time_step]
+action_sequence = [[exp(-0.1*(t-1))] for t in time_step]
 sim_states, sim_measurements = run_simulation(kf, x0, action_sequence)
 # run kalman filter
 P0 = Matrix{Float64}(I, 3, 3)
@@ -36,7 +36,7 @@ v = [x[2] for x in sim_states]
 a = [x[3] for x in sim_states]
 x = hcat(p, v, a)
 # plot
-plot(time_step, [x[2:end, 1] x[2:end, 2] x[2:end, 3]], label = ["simulated p" "simulated v" "simulated a"], legend=:bottomleft)
+plot(time_step, s[x[2:end, 1] x[2:end, 2] x[2:end, 3]], label = ["simulated p" "simulated v" "simulated a"], legend=:bottomleft)
 plot!(time_step, [μ[2:end, 1] μ[2:end, 2] μ[2:end, 3]], label = ["filtred p" "filtered v" "filtered a"], legend=:bottomleft)
 xlabel!("time step (t)")
 ## Plot noise covariance loss
@@ -65,7 +65,7 @@ Aest = randn(3, 3)
 Best = randn(3, 1)
 
 est_kf = KalmanFilter(Aest, Best, Q, H, R)
-opt = ADAM(0.0001)
+opt = ADAM(0.00002)
 
 function run_gradient(filter::KalmanFilter, action_history, measurement_history)
     s_grad = [State([0.0; 0.0; 0.0], Matrix{Float64}(I, 3, 3))]
@@ -77,10 +77,11 @@ function run_gradient(filter::KalmanFilter, action_history, measurement_history)
     for (u, y) in ProgressBar(zip(action_history, measurement_history))
         old_A = copy(filter.A)
         s = s_grad[end]
-        for i in 1:100
+        # s_old = s_grad[end]
+        for i in 1:400
             # print(x_grad, "\n")
-            sp = prediction(filter, s, u)
-            s = correction(filter, sp, y)
+            s = prediction(filter, s, u)
+            s = correction(filter, s, y)
             dA, = gradient(() -> mse_loss(filter, s, u, y), Params([filter.A]))
             # filter.A += η*dA
             update!(opt, filter.A, dA)
@@ -108,4 +109,60 @@ p2 = plot(time_step[250:end], L[250:end], title="A matrix loss")
 p3 = plot(time_step, (x[2:end, :]-μ[2:end, :]).^2, title="KF vs. sim error")
 p4 = plot(time_step[250:end], (x[251:end, :]-μgrad[251:end, :]).^2, title="grad vs. sim error")
 plot(p1, p2, p3, p4, layout=l, titlefont = font(12), size=(1000, 700))
+xlabel!("time step (t)")
+
+##
+
+using Zygote, Statistics
+
+Aest = randn(3, 3)
+Best = randn(3, 1)
+
+est_kf = KalmanFilter(Aest, B, Q, H, R)
+opt = ADAM(0.001)
+
+function kalman_mse(filter::KalmanFilter, s0::State, action_history, measurement_history)
+    s = s0
+    l = 0.0
+    N = length(action_history)
+    for (u, y) in zip(action_history, measurement_history)
+        s = prediction(filter, s, u)
+        s = correction(filter, s, y)
+        l += mse_loss(filter, s, u, y)
+    end
+    return l/N
+end
+
+epochs = 400
+
+
+function run_gradient(filter::KalmanFilter, epochs, s0::State, action_history, measurement_history)
+    @assert length(action_history) == length(measurement_history)
+    # loss_hist = []
+    all_A = []
+    for i in  ProgressBar(1:epochs)
+        # ps = Flux.params(filter.A)
+        # println(ps)
+        # gs = gradient(ps) do
+        #     kalman_mse(filter, s0, action_history, measurement_history)
+        # end
+        gs, = gradient(() -> kalman_mse(filter, s0, action_history, measurement_history), params(filter.A))
+        update!(opt, filter.A, gs)
+    end
+    return all_A
+end
+
+all_A = run_gradient(est_kf, epochs, s0, action_sequence, sim_measurements)
+grad_states = run_filter(est_kf, s0, action_sequence, sim_measurements)
+
+μgrad, Σgrad = unpack(grad_states)
+
+l = @layout [a{0.7h};grid(1, 2)]
+p1 = plot(time_step, [x[2:end, 1] x[2:end, 2] x[2:end, 3]], label = ["simulated p" "simulated v" "simulated a"], legend=:bottomright)
+p1 = plot!(time_step, [μ[2:end, 1] μ[2:end, 2] μ[2:end, 3]], label = ["filtered p" "filtered v" "filtered a"], legend=:bottomright)
+p1 = plot!(time_step, [μgrad[2:end, 1] μgrad[2:end, 2] μgrad[2:end, 3]], label = ["learned p" "learned v" "learned a"], legend=:bottomright)
+# p2 = plot(time_step[250:end], L[250:end], title="A matrix loss")
+p3 = plot(time_step, (x[2:end, :]-μ[2:end, :]).^2, title="KF vs. sim error")
+p4 = plot(time_step[250:end], (x[251:end, :]-μgrad[251:end, :]).^2, title="grad vs. sim error")
+plot(p1, p3, p4, layout=l, titlefont = font(12), size=(1000, 700))
 xlabel!("time step (t)")
